a57074180d712cce2013c498b2d3dfd648ec7274
lzl911004 <37526328+lzl911004@users.noreply.github.com>
2020-01-30 17:23:53 +1100

    2020.1.30 updated

---
+++ lee-react-store/README.md
@@ -0,0 +1,33 @@
+## 说明
+
+该项目为[《React 边学边做》]()课程项目，仅用于学习，禁止商业用途.
+
+## 使用
+
+1. 下载到本地
+
+2. 切换到项目根目录
+
+3. 项目初始化
+
+```shell
+npm install
+# or
+yarn install
+```
+
+4. 运行
+
+```shell
+npm run dev
+# or
+yarn dev
+```
+
+> ps: 项目请求数据需要 [react-store-api](https://github.com/lirenmi/react-store-api) 项目提供
+
+## 关于作者
+
+我是 _密叔_
+
+![微信](https://tva1.sinaimg.cn/large/006y8mN6ly1g8lu8qgv7bj30by0bydgn.jpg)
---
+++ lee-react-store/src/pages/AboutMe.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+const AboutMe = () => (
+    <React.Fragment>
+   <section class="hero is-info is-bold">
+  <div class="hero-body ">
+    <div class="container">
+      <h1 class="title">
+        Hello, this is Lee
+      </h1>
+      <h2 class="subtitle">
+        Feel free to try
+      </h2>
+      
+    </div>
+  </div>
+</section>
+      <section class="section">
+      <div class="container">
+      <h1 class="title">Admin Account</h1>
+      <h2 class="subtitle">
+       <strong>Email:</strong> lee@gmail.com
+      </h2>
+      <h2 class="subtitle">
+       <strong>Password:</strong> iamamaster
+      </h2>
+      <h2></h2>
+      <h1 class="title">Guest</h1>
+      <h2 class="subtitle">
+        If you wana try as a guest, please <strong>Register</strong>
+      </h2>
+      </div>
+    </section>
+  </React.Fragment>
+);
+
+export default AboutMe;
